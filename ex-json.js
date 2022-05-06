d37383f99f485f6e925c78de30f7d70e75f4c922
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-06 17:42:39 +0200

    Update ex-json.js

--- ex-json.js
+++ ex-json.js
@@ -1,3 +1,3 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
+fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?room=A')
   .then((response) => response.json())
   .then((json) => console.log(json));
