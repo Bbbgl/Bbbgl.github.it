2034c978453c514958ec38f3604da8bb7ae3d456
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-07 15:34:19 +0200

    Update ex-jsonnew.js

--- ex-jsonnew.js
+++ ex-jsonnew.js
@@ -1,4 +1,4 @@
-const API_KEY = room=A;
+const API_KEY = 'room=A';
 fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?' + API_KEY)
   .then((response) => response.json())
   .then((json) => console.log(json));
