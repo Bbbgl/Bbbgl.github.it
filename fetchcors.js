f4f849b3254477f936542aaaa6e8b155ac8a9cc2
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-09 12:55:15 +0200

    Update and rename fetchnocorssec.js to fetchcors.js

--- fetchcors.js
+++ fetchcors.js
@@ -1,4 +1,4 @@
-fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?room=A', {mode: 'no-cors'})
+fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?room=A')
 .then((response)=> {
   console.log(response.json) 
 }).catch(function(error) {  
