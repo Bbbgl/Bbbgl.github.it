ed666cc9ebcdd1f8a7c521fc22897c78711d16a5
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-09 18:25:10 +0200

    Update fetchcors.js

--- fetchcors.js
+++ fetchcors.js
@@ -1,6 +1,6 @@
 fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?room=A')
 .then((response)=> {
-  console.log(response.json) 
+  console.log(response) 
 }).catch(function(error) {  
   console.log('Request failed', error)  
 });
