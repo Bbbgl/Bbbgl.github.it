bd74528811f012ee97545a933b8a6679f459d57b
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-09 13:01:16 +0200

    Update fetchnocors.js

--- fetchnocors.js
+++ fetchnocors.js
@@ -1,1 +1,8 @@
-
+fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php?room=A', {mode: 'no-cors'})
+.then((response)=> {
+  console.log(response.json) 
+}).catch(function(error) {  
+  console.log('Request failed', error)  
+});
+© 2022 GitHub, Inc.
+Terms
