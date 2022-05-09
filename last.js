eb863dbd55ec6c76d67103a971d3b3847377135c
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-09 12:00:46 +0200

    Update last.js

--- last.js
+++ last.js
@@ -1,4 +1,4 @@
 fetch('https://kdmg.dii.univpm.it/iot/mobile/ar/example/query.php', { mode: 'no-cors'})
   .then(response => response.text())
-  .then(result => console.log('result'))
+  .then(result => console.log('result', result))
   .catch(error => console.log('error', error));
